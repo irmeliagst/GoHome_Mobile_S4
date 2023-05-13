6ce3ddb24c8a0e267a3c64bf0c6435ef57623538
ekastn15 <ekastn1506@gmail.com>
2023-05-13 08:48:53 +0700

    login

--- app/src/main/java/com/example/gohome_mobile_s4/LoginActivity.java
+++ app/src/main/java/com/example/gohome_mobile_s4/LoginActivity.java
@@ -2,19 +2,25 @@ package com.example.gohome_mobile_s4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.gohome_mobile_s4.Model.login.Data;
+import com.example.gohome_mobile_s4.retrofit.RetrofitClient;
 import com.google.android.material.textfield.TextInputEditText;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
     TextView txtRegister;
     TextInputEditText emailEdit, passEdit;
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +40,39 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, BottomnavActivity.class);
-                startActivity(intent);
-                finish();
+                if (v.getId() == R.id.btnLogin) {
+                    String email = emailEdit.getText().toString();
+                    String password = passEdit.getText().toString();
+                    login(email, password);
+                } else if (v.getId() == R.id.txtRegister) {
+                    Intent intent = new Intent(LoginActivity.this, RegisActivity.class);
+                    startActivity(intent);
+                }
             }
         });
+    }
+    private void login(String email, String password) {
+        Call<Data> loginCall = RetrofitClient.getInstance().getMyApi().login(email, password);
+        loginCall.enqueue(new Callback<Data>() {
+            @Override
+            public void onResponse(Call<Data> call, Response<Data> response) {
+                if (response.body() != null && response.body().isSuccess() && response.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, response.body().getEmail(), Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, BottomnavActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                } else {
+                    Toast.makeText(LoginActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Data> call, Throwable t) {
+                Toast.makeText(LoginActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                Log.d("DATA", t.toString());
+            }
+       });
     }
 }
--- app/src/main/java/com/example/gohome_mobile_s4/retrofit/ApiInterface.java
+++ app/src/main/java/com/example/gohome_mobile_s4/retrofit/ApiInterface.java
@@ -1,20 +1,30 @@
 package com.example.gohome_mobile_s4.retrofit;
 
-import com.example.gohome_mobile_s4.MenungguFragment;
 import com.example.gohome_mobile_s4.Model.HomeModel;
 import com.example.gohome_mobile_s4.Model.MenungguModel;
-import com.example.gohome_mobile_s4.retrofit.RetrofitClient;
+import com.example.gohome_mobile_s4.Model.login.Data;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiInterface {
-    String BASE_URL = "http://192.168.1.25:8000/api/";
+    String BASE_URL = "http://192.168.1.58:8000/api/";
+
+    @FormUrlEncoded
+    @POST("login")
+    Call<Data>login(
+            @Field("email") String email,
+            @Field("password") String password);
     @GET("kamar")
     Call<List<HomeModel>> getKamar();
     @GET("transaksi")
     Call<List<MenungguModel>> getMenunggu();
+
+
     // lets make our model class of json data .
 }
