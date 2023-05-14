714218e316adff646008c8e981930c0a8b0bf578
ekastn15 <ekastn1506@gmail.com>
2023-05-15 00:10:45 +0700

    ubah desain booking+beranda

--- app/src/main/java/com/example/gohome_mobile_s4/BookingActivity.java
+++ app/src/main/java/com/example/gohome_mobile_s4/BookingActivity.java
@@ -23,16 +23,14 @@ import java.util.Calendar;
 import java.util.Locale;
 
 public class BookingActivity extends AppCompatActivity {
-    Button btnCheckin, btnCheckout, btnTf;
-    TextView txt_nama, txt_nik, txt, txt_nomor, txt_Jkamar, tgl_checkin, tgl_checkout, txt_total, note;
+    Button btnTf;
+    TextView txt_nama, txt_nik, txt, txt_nomor, txt_Jkamar, tgl_checkin, tgl_checkout, txt_total, note, txt_checkin, txt_checkout;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R
-                .layout.activity_booking);
-        btnCheckin = findViewById(R.id.btncheckin);
-        btnCheckout = findViewById(R.id.btncheckout);
+        setContentView(R.layout.activity_booking);
+
         btnTf = findViewById(R.id.btnTf);
         txt = findViewById(R.id.txt);
         txt_Jkamar = findViewById(R.id.txt_Jkamar);
@@ -46,42 +44,7 @@ public class BookingActivity extends AppCompatActivity {
 
         // Menghubungkan variabel dengan view
         // Menambahkan onClickListener ke btnCheckin
-        btnCheckin.setOnClickListener(view -> {
-            Calendar calendar = Calendar.getInstance();
-            int year = calendar.get(Calendar.YEAR);
-            int month = calendar.get(Calendar.MONTH);
-            int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
-            // Membuka date picker dialog saat btnCheckin diklik
-            DatePickerDialog datePickerDialog = new DatePickerDialog(
-                    BookingActivity.this,
-                    (datePicker, year1, month1, day) -> {
-                        month1 += 1; // Januari dimulai dari 0
-                        String selectedDate = String.format(Locale.getDefault(), "%02d/%02d/%d", day, month1, year1);
-                        tgl_checkin.setText(selectedDate); // Menampilkan tanggal yang dipilih pada TextView checkin
-                    },
-                    year, month, dayOfMonth);
-            datePickerDialog.show();
-        });
 
-        // Menambahkan onClickListener ke btnCheckout
-        btnCheckout.setOnClickListener(view -> {
-            Calendar calendar = Calendar.getInstance();
-            int year = calendar.get(Calendar.YEAR);
-            int month = calendar.get(Calendar.MONTH);
-            int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
-            // Membuka date picker dialog saat btnCheckout diklik
-            DatePickerDialog datePickerDialog = new DatePickerDialog(
-                    BookingActivity.this,
-                    (datePicker, year1, month1, day) -> {
-                        month1 += 1; // Januari dimulai dari 0
-                        String selectedDate = String.format(Locale.getDefault(), "%02d/%02d/%d", day, month1, year1);
-                        tgl_checkout.setText(selectedDate); // Menampilkan tanggal yang dipilih pada TextView checkout
-                    },
-                                year,
-                                month,
-                                dayOfMonth);
-                        datePickerDialog.show();
-        });
 
         btnTf.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/gohome_mobile_s4/homeFragment.java
+++ app/src/main/java/com/example/gohome_mobile_s4/homeFragment.java
@@ -1,5 +1,6 @@
 package com.example.gohome_mobile_s4;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -10,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.gohome_mobile_s4.Adapter.HomeAdapter;
@@ -20,7 +23,9 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.sql.ResultSet;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -29,6 +34,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class homeFragment extends Fragment {
+    Button btnCheckin, btnCheckout;
+    TextView set_checkin, set_checkout;
     private RecyclerView recyclerView;
     private HomeAdapter myAdapter;
     private List<HomeModel> kamarList;
@@ -44,11 +51,52 @@ public class homeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        btnCheckin = view.findViewById(R.id.btncheckin);
+        btnCheckout = view.findViewById(R.id.btncheckout);
         recyclerView = view.findViewById(R.id.kamar_tampil);
         layoutManager = new LinearLayoutManager(getContext());
         layoutManager.setOrientation(RecyclerView.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
         getKamar();
+        btnCheckin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+            // Membuka date picker dialog saat btnCheckin diklik
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), (datePicker, year1, month1, day) -> {
+                        month1 += 1; // Januari dimulai dari 0
+                        String selectedDate = String.format(Locale.getDefault(), "%02d/%02d/%d", day, month1, year1);
+                        set_checkin.setText(selectedDate); // Menampilkan tanggal yang dipilih pada TextView checkin
+                    },
+                    year, month, dayOfMonth);
+            datePickerDialog.show();
+            }
+        });
+
+        // Menambahkan onClickListener ke btnCheckout
+        btnCheckout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+            // Membuka date picker dialog saat btnCheckout diklik
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(),
+                    (datePicker, year1, month1, day) -> {
+                        month1 += 1; // Januari dimulai dari 0
+                        String selectedDate = String.format(Locale.getDefault(), "%02d/%02d/%d", day, month1, year1);
+                        set_checkout.setText(selectedDate); // Menampilkan tanggal yang dipilih pada TextView checkout
+                    },
+                    year,
+                    month,
+                    dayOfMonth);
+            datePickerDialog.show();
+            }
+//
+        });
         return view;
     }
 
